dbee768f529768d42f70685d7d6bf5f097578b91
qvant <qvant86@gmail.com>
2019-02-23 15:54:37 +0300

    minor fixes

--- loader_functions/data_loaders.py
+++ loader_functions/data_loaders.py
@@ -3,7 +3,7 @@ import shelve
 
 
 def save_game(player, entities, game_map, message_log, game_state):
-    with shelve.open('saves/savegame.dat', 'n') as data_file:
+    with shelve.open('savegame.dat', 'n') as data_file:
         data_file['player_index'] = entities.index(player)
         data_file['entities'] = entities
         data_file['game_map'] = game_map
@@ -13,10 +13,10 @@ def save_game(player, entities, game_map, message_log, game_state):
 
 
 def load_game():
-    if not os.path.isfile('savegame.dat'):
+    if not os.path.isfile('savegame.dat.dat'):
         raise FileNotFoundError
 
-    with shelve.open('saves/savegame.dat', 'r') as data_file:
+    with shelve.open('savegame.dat', 'r') as data_file:
         player_index = data_file['player_index']
         entities = data_file['entities']
         game_map = data_file['game_map']
